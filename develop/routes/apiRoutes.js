9bd09301e4c42e106ca2d097534a57b5f19a64f3
Erica Roquemore <enroquemore@gmail.com>
2021-05-11 00:31:43 -0400

    updated paths in server.js

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const generateUniqueId = require('generate-unique-id');
+// const generateUniqueId = require('generate-unique-id');
 
 
 
@@ -8,44 +8,44 @@ module.exports = (app) => {
 
 // Getting the API data from db.json file
 app.get('/api/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, '../data/noteData.json'));
+    res.sendFile(path.join(__dirname, '../dp/dp.json'));
 })
 
 
 // Posting new notes to the API via the db.json file
-app.post('/api/notes', function(req, res) {
-    let newNote = req.body;
-    const myID = generateUniqueId({
-        length: 5,
-        useLetters: false
-      });
-    newNote.id = myID;
-    console.log(newNote);
-    var data = fs.readFileSync('./data/noteData.json');
-    var myObj = JSON.parse(data);
-    myObj.push(newNote)  
-    newNote = JSON.stringify(myObj);
-    fs.writeFile('./data/noteData.json', newNote, err => {
-        if (err) throw err;
-        console.log('NEW NOTE ADDED!');
-    }); 
-    res.json(newNote);
-})
+// app.post('/api/notes', function(req, res) {
+//     let newNote = req.body;
+//     const myID = generateUniqueId({
+//         length: 5,
+//         useLetters: false
+//       });
+//     newNote.id = myID;
+//     console.log(newNote);
+//     var data = fs.readFileSync('./data/noteData.json');
+//     var myObj = JSON.parse(data);
+//     myObj.push(newNote)  
+//     newNote = JSON.stringify(myObj);
+//     fs.writeFile('./data/noteData.json', newNote, err => {
+//         if (err) throw err;
+//         console.log('Note Added!');
+//     }); 
+//     res.json(newNote);
+// })
 
 
 
-    app.delete('/api/notes/:id', function(req, res) {
-        // res.send('DELETE REQUEST CALLED')
-        const { id } = req.params;
-        var data = fs.readFileSync('./data/noteData.json');
-        var myObj = JSON.parse(data);
-        const deletedNote = myObj.find(data => id === data.id);
-        index = myObj.indexOf(deletedNote);
-        myObj.splice(index, 1);
-        newObj = JSON.stringify(myObj)
-        fs.writeFile('./data/noteData.json', newObj, function(err){
-            if (err) throw err;
-        })
-        res.send('Successfully deleted Note');
-    })
+//     app.delete('/api/notes/:id', function(req, res) {
+//         // res.send('DELETE REQUEST CALLED')
+//         const { id } = req.params;
+//         var data = fs.readFileSync('./develop/dp/dp.json');
+//         var myObj = JSON.parse(data);
+//         const deletedNote = myObj.find(data => id === data.id);
+//         index = myObj.indexOf(deletedNote);
+//         myObj.splice(index, 1);
+//         newObj = JSON.stringify(myObj)
+//         fs.writeFile('./develop/dp/dp.json', newObj, function(err){
+//             if (err) throw err;
+//         })
+//         res.send('Deleted Note');
+//     })
 }
--- develop/server.js
+++ develop/server.js
@@ -1,14 +1,19 @@
+console.log("Hello Noder!");
+
 const express = require('express');
+const path = require('path');
+const uuid = require("uuid");
 
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5500;
 app.use(express.static('./public'));
-app.use(express.static('./data'));
+app.use(express.static('./db/db.json'));
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('./public'));
 
 require('./routes/htmlRoutes/htmlRoutes')(app);
 
