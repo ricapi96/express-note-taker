35ff14b93cbe70a3d5deff493cc6a4c372998306
Erica Roquemore <enroquemore@gmail.com>
2021-05-16 23:13:59 -0400

    updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('./public'));
 app.use(express.static('./db/db.json'));
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
 
