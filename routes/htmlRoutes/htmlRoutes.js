9f74debff8ce38d5cd1d11ed188666a781efe83c
Erica Roquemore <enroquemore@gmail.com>
2023-11-01 21:21:19 -0400

    updating routes

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,10 +1,14 @@
-  
 // const path = require('path');
+// const router = require('express').Router();
 
+// "/notes" responds with the notes.html file
+// router.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/notes.html'));
+// });
 
+// All other routes respond with the index.html file
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
-// module.exports = (app) => {
-//     app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-//     app.get('/notes', (req, res) => res.sendFile(path.join(process.cwd(), '/public/notes.html')));
-// }
+// module.exports = router;
